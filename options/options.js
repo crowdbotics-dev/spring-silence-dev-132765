40b955d854678290878b3018528bd316cd8ffd99
Crowdbotics <engineering@crowdbotics.com>
2024-01-30 10:26:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-silence-dev-132765",
+  url: "https://spring-silence-dev-132765.botics.co",
+  api: "https://spring-silence-dev-132765.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
